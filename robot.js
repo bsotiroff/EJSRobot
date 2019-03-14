a6d290c8dd83ce49733336cb10b55e1e085bcb8a
Brandon Sotiroff <bsotiroff1@gmail.com>
2019-03-13 20:24:21 -0500

    Initial Commit

---
+++ robot.js
@@ -0,0 +1,41 @@
+//roads described as where they come from to where they go
+const roads = [
+  "Alice's House-Bob's House",
+  "Alice's House-Cabin",
+  "Alice's House-Post Office",
+  "Bob's House-Town Hall",
+  "Daria's House-Ernie's House",
+  "Daria's House-Town Hall",
+  "Ernie's House-Grete's House",
+  "Grete's House-Farm",
+  "Grete's House-Shop",
+  'Marketplace-Farm',
+  'Marketplace-Post Office',
+  'Marketplace-Shop',
+  'Marketplace-Town Hall',
+  'Shop-Town Hall',
+];
+
+//build graph is a constructor function
+// it creates an object called graph
+//
+
+function buildGraph(edges) {
+  let graph = Object.create(null);
+  function addEdge(from, to) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
+    }
+  }
+  for (let [from, to] of edges.map(r => r.split('-'))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+  return graph;
+}
+
+const roadGraph = buildGraph(roads);
+
+console.log(roadGraph);
